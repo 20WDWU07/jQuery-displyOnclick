a6b0d5b9d80205d78d65c7982491dd94b5f9f72d
beulathamban <beulathamban@gmail.com>
2020-09-07 15:09:00 +1200

    cart with total amount

--- js/script.js
+++ js/script.js
@@ -1,12 +1,55 @@
 console.log('js');
+
+$('#cart').hide();
+
+var total =0; //initializing the numeric variable
+
 $(document).ready(function(){
-  // $("button").click(function(){
-  //   $("p").hide();
-  // });
+  $('#back').on('click',function(){
+    $('#menu').show();
+    $('#cart').hide();
+  })
+
   $('.menu-button').on('click', function(){
-     console.log(this.id);
-    var itemName = this.id; //stogin the value to a variable
+
+    //item name from id of button being clicked;
+    //this refers to what is being clicked
+
+    console.log(this.id);
+    // $('#items').append ('<br>' + this.id)
+    var itemName = this.id; //storing the value to a variable
     $('#items').append ('<br>' + itemName);
-    $('#price').append ('<br>' + itemName);
-  });
+
+    // console.log($('.menu-button').index(this));
+    var myIndex = $('.menu-button').index(this);
+    console.log(myIndex);
+
+    var itemPrice = document.getElementsByClassName('itemPrice');
+    console.log(itemPrice);
+    console.log(itemPrice.length);
+
+    //searching or matching
+    for (var i=0; i< itemPrice.length;i++){
+      // console.log(itemPrice[i].innerHTML);
+      if (myIndex == i){
+        // console.log('yes');//
+        console.log(itemPrice[i].innerHTML);
+        var price = itemPrice[i].innerHTML;
+        $('#price').append('<br>' + price);
+        total += parseFloat(price);
+        console.log(total);
+      } //if ends
+} //for loop ends
+
+}); //.menu-button
+$('#cartIcon').click(function(){
+  console.log('clicked');
+$('#cart').show();
+$('#menu').hide();
 });
+
+$('#calculate').on('click', function(){
+  document.getElementById('amount').innerHTML = '$' + total.toFixed(2);
+});
+
+});//document.ready() ends
